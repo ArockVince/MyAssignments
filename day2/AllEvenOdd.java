acd525da53b58c919813f2c2a7430c95f491d5a5
ArockVince <119476395+ArockVince@users.noreply.github.com>
2022-11-30 11:30:56 +0530

    Add files via upload
    day2

---
+++ day2/AllEvenOdd.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class AllEvenOdd {
+	
+	 public static void main(String[] args) 
+		{
+		  for(int i=1;i<=10;i++)
+		  {
+			  if(i%2==0)
+				{
+					System.out.println("The given number is even " + i);
+				}
+				else {
+					System.out.println("The given number is odd " + i);
+				}
+		  }
+		  }
+		}
+		
+
---
+++ day2/OddOrEven.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class OddOrEven 
+{ 
+	 public static void main(String[] args) 
+	{
+		int num = 10;
+		
+	if(num%2==0)
+	{
+		System.out.println("The given number is even");
+	}
+	else {
+		System.out.println("The given number is odd");
+	}
+		
+	
+}
+
+}
