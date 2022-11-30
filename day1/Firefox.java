5f027c6bb2c4ac90b66f58e3fd0080d140500b33
ArockVince <119476395+ArockVince@users.noreply.github.com>
2022-11-30 11:28:16 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	float version = 100.2f;
+	String name = "firefox";
+	boolean visible = true;
+	int year = 1998;
+	char logo = 'm';
+	
+	public static void main(String[] args) {
+		Firefox objFirfox = new Firefox();
+		System.out.println(objFirfox.version);
+		System.out.println(objFirfox.name);
+		System.out.println(objFirfox.year);
+		System.out.println(objFirfox.visible);
+		System.out.println(objFirfox.logo);
+		
+		}
+}
---
+++ day1/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Mobile {
+	
+	public void sendSms()
+	{
+		System.out.println("Happy to learn Java");
+	}
+	public void makeCall()
+	{
+		System.out.println("calling my friend");
+		
+	}
+	 public static void main(String[] args)
+	 {
+		 Mobile objMobile = new Mobile();
+		 objMobile.sendSms();
+		 objMobile.makeCall();
+	 }
+	 
+	
+
+}
